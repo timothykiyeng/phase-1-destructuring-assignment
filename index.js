79788f62117ed78cada86e5add7a651198550459
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-20 11:23:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = "cow horse sheep pig chicken";
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+console.log(moo, neigh, baa, oink, cluck);
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ");
+console.log(bessie, dolly, babe, little);
+
+const [blackAndWhite, , black, pink] = farmAnimals.split(" ");
+console.log(blackAndWhite, black, pink);
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log(red, orange, yellow, green, blue, indigo, violet);
+
+const [r, o, y, g, b, , v] = colors;
+console.log(r, o, y, g, b, v);
+
+const [, , , , , indg] = colors;
+console.log(indg);
 
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit",
 };
 
+const { muppetName, color, song, job, partner } = muppet;
+console.log(muppetName);
+console.log(color);
+console.log(song);
+console.log(job);
+console.log(partner);
+
 const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
+  nestedName: "Kermit",
+  nestedColor: "green",
   album: {
     theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
+      song1: "Rainbow Connection",
+      song2: "Moving Right Along",
+      song3: "Never Before, Never Again",
+      song4: "I Hope That Something Better Comes Along",
     },
   },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy",
 };
+
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+console.log(song2);
+console.log(song4);
+
+const { nestedJob, nestedPartner } = nestedMuppet;
+console.log(nestedJob);
+console.log(nestedPartner);
 
 // Strings
 
@@ -39,7 +70,7 @@ const nestedMuppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
